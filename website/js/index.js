78e1aeb7749572c71b59ed64b2fe9e6dfaada3dc
Logan Uibel <lbuibel@gmail.com>
2020-02-25 11:28:13 -0700

    setup hamburger nav

--- website/js/index.js
+++ website/js/index.js
@@ -1,1 +1,22 @@
 // Custom JS here
+const navOpen = () => {
+    const burger = document.querySelector('#burger')
+    const nav = document.querySelector('.nav-window')
+
+    burger.addEventListener('click', ()=> {
+        console.log('test')
+        nav.classList.toggle('nav-active')
+    })
+}
+
+const navClose = () => {
+    console.log('closed')
+    const close = document.querySelector('#close')
+    const nav = document.querySelector('.nav-window')
+    close.addEventListener('click', () => {
+        nav.classList.remove('nav-active')
+    })
+}
+
+navOpen()
+navClose()
