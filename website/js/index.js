07f51ba2d619946f792f901455f0fc2738ac939e
Logan Uibel <lbuibel@gmail.com>
2020-02-25 17:48:05 -0700

    setup navigation image and animation

--- website/js/index.js
+++ website/js/index.js
@@ -2,10 +2,21 @@
 const navOpen = () => {
     const burger = document.querySelector('#burger')
     const nav = document.querySelector('.nav-window')
-
+    const navLinks = document.querySelectorAll('.nav-links li')
+    // opens navigation page
     burger.addEventListener('click', ()=> {
         console.log('test')
         nav.classList.toggle('nav-active')
+    })
+    // fade in links
+    navLinks.forEach((link, index) => {
+        // fades in each element with time delay
+        if (link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = `nav-link-fade 1s ease forwards ${index / 15 + 1.5}s`
+        }
+        console.log(index/7 + 1.5)
     })
 }
 
